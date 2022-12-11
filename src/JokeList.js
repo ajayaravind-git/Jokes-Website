9d262135f01c83a756b7726a6c7baef43f904a2e
Ajay Aravind <ajayaravind.me@gmail.com>
2022-12-11 23:46:52 +0530

    upvote and downvote buttons implemented

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import './JokeList.css'
+import Joke from './Joke';
 class JokeList extends Component {
     static defaultProps = {
         numJokesToGet: 10
@@ -13,13 +14,14 @@ class JokeList extends Component {
 
         }
         this.handleClick = this.handleClick.bind(this);
+        this.changeVotes = this.changeVotes.bind(this);
     }
 
     async componentDidMount() {
         let jokes = [];
         while (jokes.length < this.props.numJokesToGet) {
             let res = await axios.get('https://icanhazdadjoke.com/', { headers: { Accept: "application/json" } })
-            let joke = { joke: res.data.joke, votes: 0 }
+            let joke = { joke: res.data.joke, votes: 0, id: res.data.id }
             jokes.push(joke)
         }
         this.setState({
@@ -27,20 +29,29 @@ class JokeList extends Component {
         })
 
     }
+    changeVotes(id, delta) {
+        this.setState(st => ({
+            jokes: st.jokes.map(j => j.id === id ? { ...j, votes: j.votes + delta } : j)
+        }));
+
+    }
+
     handleClick() {
         this.setState({ refreshToggle: !this.state.refreshToggle })
 
     }
     render() {
-        let jokes = this.state.jokes.map(jk => <div>{jk.joke}- {jk.votes}</div>)
+        let jokes = this.state.jokes.map(jk => <Joke jokedisplay={jk.joke} key={jk.id} downVote={() => this.changeVotes(jk.id, -1)} upVote={() => this.changeVotes(jk.id, +1)} id={jk.id} votes={jk.votes} />)
         return <div className='JokeList'>
             <div className='JokeList-sidebar'> <h1 className='JokeListTitle'><span>Dad</span> Jokes</h1>
                 <img className='JokeListImage' src='https://i.pinimg.com/originals/d5/9c/90/d59c9002030448f1193adf7d7600a52a.png' />
                 <button className='JokeListButton' onClick={this.handleClick}>New Jokes</button>
+
             </div>
 
             <div className='JokeList-jokes'>
-                {jokes}</div>
+                {jokes}
+            </div>
 
 
         </div>
